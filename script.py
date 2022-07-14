81724b95f3d9c7fa8212893840bce2244185b794
shinthantkyaw7 <109136650+shinthantkyaw7@users.noreply.github.com>
2022-07-14 18:06:46 +0630

    Update script.py

--- script.py
+++ script.py
@@ -1,65 +1,39 @@
 class script(object):
 
 
-    START_MSG = """ <b>မင်္ဂလာပါ   {} 
-
-ကျွန်တော်ကတော့ Auto Filter Bot ပါ
-
-ကျွန်တော်ကို Group ‌နဲ့ Movie Channel များတွင် Admin ထည့်ပြီး မည်သူမဆိုလွတ်လပ်စွာအသုံးပြုနိုင်ပါတယ်။..
-
-@KOPAING15 <i>-Contact Me</i></b>"""
+    START_MSG = """ <b>ဟေလူ {} 
+ဘာလာရှာတာလဲ 
+လိုချင်တာပြောနော်
+Delivery နဲ့ ပါဆယ်ပိုပေးလိုက်မယ်။ Ok လား? 😂😂
+@Channel_X_Movies <i>-Contact Me</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
-
 <i>
 * စီမံခန့်ခွဲသူအခွင့်အရေးဖြင့် သင့်အဖွဲ့သို့ ဘော့တ်ကို ထည့်ပါ။
-
 * သင်ချိတ်ဆက်လိုသော ချန်နယ်များသို့ ဘော့တ်ကို ထည့်ပါ။။ <b> အခွင့်အရေးအားလုံး admin ပေးပါ</b>!
 </i>
-
-
 <b>Bot Commands - Works in Group only</b>
 (You need to be a Auth User in order to use these commands)
-
 * <code>/add channelid</code>  -  Links channel to your group.
 or
 * <code>/add @channelusername</code> - Links channel to your group.
-
 <i>NOTE : You can get your channel ID from @MT_ID_BOT </i>
-
-
 * <code>/del channelid</code>  -  Delinks channel from group
 or
 * <code>/del @channelusername</code>  -  Delinks channel from group
-
 <i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
 * <code>/delall</code>  -  Removes all connected channels and filters from group!
-
 <i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
 * <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
 No need add each filter again!
 Bot will automatically search for your files and give links to that!
-
-
 <b>© @KOPAING15</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : KO PAing </b>
+    ABOUT_MSG = """⭕️<b>My Name : Channel X </b>
     
-⭕️<b>Creater :</b> @KOPAING15
-
+⭕️<b>Creater :</b> @Channel_X_Movies
 ⭕️<b>Language :</b> <code>Python3</code>
-
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://t.me/mksmainchannel'>Video Link</a> 
-
-"""
+⭕️<b>Tutorial Video :</b> <a href='https://t.me/+XZpJfRc0kDkxZmJl'>Video Link</a> 
